904ae581d8ea04b70040b5a634e6eb0f30c6907c
CarlosEspinal <carlosespinale@Carloss-MBP.home>
2016-10-03 12:33:10 -0400

    implimented devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #required for heroku
+  #not to set this to set for actual host
+  config.action_mailer.default_url_options = { host: => 'https://travel-always.herokuapp.com/' }
+
+
 end
